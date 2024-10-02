08f0fb6788f9f62dba88d4df5cb520e97bcb419b
Haobo Ren <haobo.ren@mail.utoronto.ca>
2024-10-01 20:36:33 -0400

    modify scripts

--- scripts/2-data-cleaning.R
+++ scripts/2-data-cleaning.R
@@ -4,6 +4,14 @@
 # Date: 23 September 2024
 # Contact: haobo.ren@mail.utoronto.ca
 
+#### Workspace setup ####
+library(tidyverse)
+library(janitor)
 
 data <- read.csv("data/raw_data/players_20.csv")
-name_cleaned_data <- clean_names(data)
+cleaned_data <- clean_names(data)
+cleaned_data <- cleaned_data |> 
+  select(
+  
+)
+write.csv(cleaned_data, "data/cleaned_data/cleaned_data.csv")
