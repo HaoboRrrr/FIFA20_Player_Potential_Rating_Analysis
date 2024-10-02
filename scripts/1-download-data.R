a032db7044abc62cb1ec26c71e7c8f482f89f9d4
任浩博 <renhaobo@dfsoft.com.cn>
2024-10-01 21:04:34 -0400

    hahaha

--- scripts/1-download-data.R
+++
@@ -1,8 +0,0 @@
-#### Preamble ####
-# Purpose: Downloads and saves the FIFA 20 complete player dataset
-# Author: Haobo Ren
-# Date: October 9 2024
-# Contact: haobo.ren@mail.utoronto.ca
-# Source Data avialable at https://www.kaggle.com/datasets/stefanoleone992/fifa-20-complete-player-dataset?resource=download&select=players_20.csv
-
-
